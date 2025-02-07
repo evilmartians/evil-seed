0bc973e1ae17941e4f3942ef0b989e3f195d7a42
Andrey Novikov <envek@envek.name>
2025-02-07 20:27:19 +0900

    Simplify one column tables handling

--- lib/evil_seed/relation_dumper.rb
+++ lib/evil_seed/relation_dumper.rb
@@ -169,8 +169,7 @@ module EvilSeed
     # @return [Array<Hash{String => String, Integer, Float, Boolean, nil}>]
     def fetch_attributes(relation)
       relation.pluck(*model_class.column_names).map do |row|
-        row = [row] if model_class.column_names.size == 1
-        Hash[model_class.column_names.zip(row)]
+        Hash[model_class.column_names.zip(Array(row))]
       end
     end
 
